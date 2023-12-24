6d6daea3a5bd0297848cbd7d528990d821de819e
dtapps <li.gc@icloud.com>
2023-12-24 17:16:14 +0800

    - 增加百度云接口

--- cloud/rpc.2.0.ai_custom.v1.wenxinworkshop.chat.xuanyuan_70b_chat.go
+++ cloud/rpc.2.0.ai_custom.v1.wenxinworkshop.chat.xuanyuan_70b_chat.go
@@ -8,22 +8,20 @@ import (
 )
 
 type Rpc2AiCustomV1WenxinworkshopChatXuanyuan70bChatResponse struct {
-	LogId  string `json:"log_id"`
-	Result struct {
-		TemplateId         int         `json:"templateId"`        // prompt工程里面对应的模板id
-		TemplateName       string      `json:"templateName"`      // 模板名称
-		TemplateContent    string      `json:"templateContent"`   // 模板原始内容
-		TemplateVariables  string      `json:"templateVariables"` // 模板变量插值
-		Content            string      `json:"content"`           // 将变量插值填充到模板原始内容后得到的模板内容
-		VariableIdentifier string      `json:"variableIdentifier"`
-		Labels             interface{} `json:"labels"`
-		CreatorName        string      `json:"creatorName"`
-		Type               int         `json:"type"`
-		SceneType          int         `json:"sceneType"`
-		FrameworkType      int         `json:"frameworkType"`
-	} `json:"result"`
-	Status  int  `json:"status"`
-	Success bool `json:"success"`
+	Id               string `json:"id"`                    // 本轮对话的id
+	Object           string `json:"object"`                // 回包类型
+	Created          int    `json:"created"`               // 时间戳
+	SentenceId       int    `json:"sentence_id,omitempty"` // 表示当前子句的序号。只有在流式接口模式下会返回该字段
+	IsEnd            int    `json:"is_end,omitempty"`      // 表示当前子句是否是最后一句。只有在流式接口模式下会返回该字段
+	IsTruncated      bool   `json:"is_truncated"`          // 当前生成的结果是否被截断
+	Result           string `json:"result"`                // 对话返回结果
+	NeedClearHistory bool   `json:"need_clear_history"`    // 表示用户输入是否存在安全，是否关闭当前会话，清理历史会话信息  true：是，表示用户输入存在安全风险，建议关闭当前会话，清理历史会话信息 false：否，表示用户输入无安全风险
+	BanRound         int    `json:"ban_round"`             // 当need_clear_history为true时，此字段会告知第几轮对话有敏感信息，如果是当前问题，ban_round=-1
+	Usage            struct {
+		PromptTokens     int `json:"prompt_tokens"`     // 问题tokens数
+		CompletionTokens int `json:"completion_tokens"` // 回答tokens数
+		TotalTokens      int `json:"total_tokens"`      // tokens总数
+	} `json:"usage"` // token统计信息
 }
 
 type Rpc2AiCustomV1WenxinworkshopChatXuanyuan70bChatResult struct {
